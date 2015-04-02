7723509a402f44d0096056bccbca99f5f69279ed
Tikhon Tagunov <niptuckker@gmail.com>
2015-04-03 02:43:08 +0300

    Entities generator

---
+++ generate.php
@@ -0,0 +1,54 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\EntityGenerator;
+
+require_once "vendor/autoload.php";
+
+$libPath = __DIR__; // Set this to where you have doctrine2 installed
+// autoloaders
+// require_once $libPath . '/Doctrine/Common/ClassLoader.php';
+
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine', $libPath);
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('Entities', __DIR__);
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('Proxies', __DIR__);
+$classLoader->register();
+
+// config
+$config = new \Doctrine\ORM\Configuration();
+$config->setMetadataDriverImpl($config->newDefaultAnnotationDriver(__DIR__ . '/Entities'));
+$config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
+$config->setProxyDir(__DIR__ . '/Proxies');
+$config->setProxyNamespace('Proxies');
+
+$connectionParams = require "./db.connection.mysql.php";
+
+$em = \Doctrine\ORM\EntityManager::create($connectionParams, $config);
+
+// custom datatypes (not mapped for reverse engineering)
+$em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('set', 'string');
+$em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+
+// fetch metadata
+$driver = new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
+    $em->getConnection()->getSchemaManager()
+);
+$em->getConfiguration()->setMetadataDriverImpl($driver);
+$cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory($em);
+$cmf->setEntityManager($em);
+$classes = $driver->getAllClassNames();
+$metadata = $cmf->getAllMetadata();
+$generator = new EntityGenerator();
+$generator->setUpdateEntityIfExists(true);
+$generator->setGenerateStubMethods(true);
+$generator->setGenerateAnnotations(true);
+$generator->generate($metadata, __DIR__ . '/Entities');
+print 'Done!';
